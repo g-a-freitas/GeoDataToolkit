4f10953ff52a324e99f1ef79d11919f1b46f45aa
Gabriel Moreira <gabrielspmoreira@yahoo.com.br>
2012-11-07 12:16:10 -0200

    Fixing Copy Bat

--- GeoDataToolkit/GeoDataToolkit.FGDB/Libraries/EsriFileGeodatabaseAPI/CopyFileGDBAPI.bat
+++ GeoDataToolkit/GeoDataToolkit.FGDB/Libraries/EsriFileGeodatabaseAPI/CopyFileGDBAPI.bat
@@ -7,20 +7,20 @@ ECHO Batch Dir: %~dp0
 Set BATCHFILE_DIR=%~dp0
 
 IF (%1) EQU (x86) (
-  xcopy /Y /Q /D %BATCHFILE_DIR%\x86\FileGDBAPI*.*
-  xcopy /Y /Q /D %BATCHFILE_DIR%\x86\Esri.FileGDB*.*
+  xcopy /Y /Q /D "%BATCHFILE_DIR%\x86\FileGDBAPI*.*"
+  xcopy /Y /Q /D "%BATCHFILE_DIR%\x86\Esri.FileGDB*.*"
   GOTO :EOF
 )
 
 IF (%1) EQU (AnyCPU) (
-  xcopy /Y /Q /D %BATCHFILE_DIR%\x86\FileGDBAPI*.*
-  xcopy /Y /Q /D %BATCHFILE_DIR%\x86\Esri.FileGDB*.*
+  xcopy /Y /Q /D "%BATCHFILE_DIR%\x86\FileGDBAPI*.*"
+  xcopy /Y /Q /D "%BATCHFILE_DIR%\x86\Esri.FileGDB*.*"
   GOTO :EOF
 )
 
 IF (%1) EQU (x64) (
-  xcopy /Y /Q /D %BATCHFILE_DIR%\x64\FileGDBAPI*.*
-  xcopy /Y /Q /D %BATCHFILE_DIR%\x64\Esri.FileGDB*.*
+  xcopy /Y /Q /D "%BATCHFILE_DIR%\x64\FileGDBAPI*.*"
+  xcopy /Y /Q /D "%BATCHFILE_DIR%\x64\Esri.FileGDB*.*"
   GOTO :EOF
 )
 
